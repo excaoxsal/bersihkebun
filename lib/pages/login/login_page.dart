241667c27eed79f67dbac8afb7d60cab66a13df9
excaoxsal <excaoxsal@users.noreply.github.com>
2020-12-08 23:12:40 +0700

    tambahan08/12/2020

---
+++ lib/pages/login/login_page.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqlite/data/database-helper.dart';
+import 'package:flutter_sqlite/models/user.dart';
+import 'package:flutter_sqlite/pages/login/login_presenter.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> implements LoginPageContract {
+  BuildContext _ctx;
+  bool _isLoading = false;
+  final formKey = new GlobalKey<FormState>();
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  String _email, _password;
+
+  LoginPagePresenter _presenter;
+
+  _LoginPageState() {
+    _presenter = new LoginPagePresenter(this);
+  }
+
+  void _register() {
+    Navigator.of(context).pushNamed("/register");
+  }
+  bool _secureText = true;
+  showHide() {
+    setState(() {
+      _secureText = !_secureText;
+    });
+  }
+  void _submit() {
+    final form = formKey.currentState;
+
+    if (form.validate()) {
+      setState(() {
+        _isLoading = true;
+        form.save();
+        _presenter.doLogin(_email, _password);
+      });
+    }
+  }
+
+  void _showSnackBar(String text) {
+    scaffoldKey.currentState.showSnackBar(new SnackBar(
+      content: new Text(text),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    _ctx = context;
+    var loginBtn = new RaisedButton(
+      onPressed: _submit,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(80.0),
+      ),
+      child: new Text("Login"),
+        color: Colors.teal[200]
+
+    );
+    var registerBtn = new RaisedButton(
+      padding: const EdgeInsets.all(10.0),
+      onPressed: _register,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(80.0),
+        ),
+      child: new Text("Register"),
+        color: Colors.teal[200]
+    );
+    var loginForm = new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+
+        Container(
+          height: MediaQuery.of(context).size.height / 3,
+          decoration: BoxDecoration(
+            color: Colors.white10,
+            image: DecorationImage(
+              image: AssetImage('images/Logokeciltosca.png'),
+
+            ),
+          ),
+        ),
+        new Form(
+          key: formKey,
+          child: new Column(
+            children: <Widget>[
+              new Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: new TextFormField(
+                  onSaved: (val) => _email = val,
+                  decoration: new InputDecoration(
+                    suffixIcon: Icon(
+                      Icons.email,
+                      color: Colors.teal[300],
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.teal,
+                      ),
+                    ),
+                      labelText: "Email",
+                    labelStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: new TextFormField(
+                  obscureText: _secureText,
+                  onSaved: (val) => _password = val,
+                  decoration: new InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.teal,
+                      ),
+                    ),
+                      labelText: "Password",
+                    suffixIcon: IconButton(
+                      onPressed: showHide,
+                      icon: Icon(_secureText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        new Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: loginBtn),
+
+        registerBtn
+      ],
+    );
+
+    return new Scaffold(
+      resizeToAvoidBottomPadding: false,
+      key: scaffoldKey,
+      body: new Container(
+        child: new Center(
+          child: loginForm,
+        ),
+      ),
+    );
+  }
+  //
+
+  @override
+  void onLoginError(String error) {
+    // TODO: implement onLoginError
+    _showSnackBar("Login not successful");
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void onLoginSuccess(User user) async {
+    // TODO: implement onLoginSuccess
+    if(user.username == ""){
+      _showSnackBar("Login not successful");
+    }else{
+    _showSnackBar(user.toString());
+    }
+    setState(() {
+      _isLoading = false;
+    });
+    if(user.flaglogged == "logged"){
+      print("Logged");
+      Navigator.of(context).pushNamed("/home");
+    }else{
+      print("Not Logged");
+    }
+  }
+}
---
+++ lib/pages/login/register.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqlite/data/database-helper.dart';
+import 'package:flutter_sqlite/models/user.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => new _RegisterPageState();
+}
+
+
+
+class _RegisterPageState  extends State<RegisterPage> {
+  BuildContext _ctx;
+  bool _isLoading = false;
+  final formKey = new GlobalKey<FormState>();
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+  String _name, _username, _password, _numberphone;
+
+  bool _secureText = true;
+  showHide() {
+    setState(() {
+      _secureText = !_secureText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _ctx = context;
+    var loginBtn = new RaisedButton(
+        onPressed: _submit,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(80.0),
+        ),
+        child: new Text("Register"),
+        color: Colors.teal[200]
+
+    );
+
+
+    var loginForm = new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+
+        new Form(
+          key: formKey,
+          child: new Column(
+            children: <Widget>[
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new TextFormField(
+                  onSaved: (val) => _name = val,
+                  decoration: new InputDecoration(labelText: "Nama"),
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new TextFormField(
+                  onSaved: (val) => _numberphone = val,
+                  decoration: new InputDecoration(labelText: "Nomor HP"),
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new TextFormField(
+                  onSaved: (val) => _username = val,
+                  decoration: new InputDecoration(labelText: "Email"),
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new TextFormField(
+                  obscureText: _secureText,
+                  onSaved: (val) => _password = val,
+                  decoration: new InputDecoration(
+                      labelText: "Password",
+                    suffixIcon: IconButton(
+                      onPressed: showHide,
+                      icon: Icon(_secureText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        loginBtn
+      ],
+    );
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Register"),
+          backgroundColor: Colors.teal[300],
+      ),
+      key: scaffoldKey,
+      body: new Container(
+        child: new Center(
+          child: loginForm,
+        ),
+      ),
+    );
+  }
+
+  void _showSnackBar(String text) {
+    scaffoldKey.currentState.showSnackBar(new SnackBar(
+      content: new Text(text),
+    ));
+  }
+
+  void _submit(){
+    final form = formKey.currentState;
+
+    if (form.validate()) {
+      setState(() {
+        _isLoading = true;
+        form.save();
+        var user = new User(_name, _username, _password, null);
+        var db = new DatabaseHelper();
+        db.saveUser(user);
+        _isLoading = false;
+        Navigator.of(context).pushNamed("/login");
+      });
+    }
+  }
+}
